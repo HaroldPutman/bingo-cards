6aa49a5ccc84516b5846380903e3566b05d9614f
Harold Putman <harold.putman@lexmark.com>
2019-11-04 15:32:17 -0500

    Refactored to survive refresh. Because on iPhone if you switch away from the browser your page can get reloaded when you come back. Added a New Game button to regenerate a clean card.

--- www/bingo-card.js
+++ www/bingo-card.js
@@ -7,30 +7,78 @@ function shuffle(array) {
   }
 }
 
-function fillBoard(clues) {
-  const board = document.querySelector('.board');
+function saveBoard() {
+  const cells = document.querySelectorAll('.board > div');
+  const serialized = Array.from(cells).map((el) => {
+    return {
+      t: el.innerText,
+      m: el.classList.contains('marked')
+    }
+  });
+  localStorage.setItem('saved', JSON.stringify(serialized));
+}
+
+function buildHeading() {
+  const el = document.querySelector('.card h1');
+  const text = el.innerText;
+  el.innerText = '';
+  Array.from(text).forEach((ch) => {
+    const span = document.createElement('span');
+    span.innerText = ch;
+    el.appendChild(span);
+  });
+}
+
+function clickCell(event) {
+  this.classList.toggle('marked');
+  const rect = event.target.getBoundingClientRect();
+  const offset = (event.clientX - rect.left) / rect.width;
+  this.style.backgroundPositionX = (offset * 100)+'%';
+}
+
+function buildEmptyBoard(board) {
   for (let i = 0; i < 25; i++) {
     const cell = document.createElement('div');
     const div = document.createElement('div');
-    div.innerText = clues[i];
     cell.appendChild(div);
-    cell.addEventListener('click', function(event) {
-      this.classList.toggle('marked');
-      const rect = event.target.getBoundingClientRect();
-      const offset = (event.clientX - rect.left) / rect.width;
-      this.style.backgroundPositionX = (offset * 100)+'%';
-    });
+    cell.addEventListener('click', clickCell);
     board.appendChild(cell);
   }
 }
 
-function explodeHeading(el) {
-  const text = el.innerText;
-  el.innerText = '';
-  for (let i = 0 ; i < text.length; i++) {
-    const span = document.createElement('span');
-    span.innerText = text[i];
-    el.appendChild(span);
+function fillBoard(content) {
+  const cells = document.querySelectorAll('.board > div');
+  cells.forEach((cell, index) => {
+    const div = cell.querySelector('div');
+    div.innerText = content[index].t;
+    cell.classList.toggle('marked', content[index].m);
+  })
+}
+
+function resetBoard(cluefile) {
+  req=new XMLHttpRequest();
+  req.open("GET", cluefile ,true);
+  req.overrideMimeType("application/json");
+  req.send();
+  req.onload = function onLoad() {
+    const clues = JSON.parse(req.responseText);
+    shuffle(clues);
+    clues[12] = 'FREE';
+    fillBoard(clues.map((text) => {
+      return {
+        t: text,
+        m: false
+      };
+    }));
+  };
+}
+
+function loadBoard(cluefile) {
+  const saved = localStorage.getItem('saved');
+  if (!saved) {
+    resetBoard(cluefile);
+  } else {
+    fillBoard(JSON.parse(saved));
   }
 }
 
@@ -40,17 +88,17 @@ function ready() {
   if (searchParams.has('clues')) {
     cluefile = searchParams.get('clues');
   }
-  req=new XMLHttpRequest();
-  req.open("GET", cluefile ,true);
-  req.overrideMimeType("application/json");
-  req.send();
-  req.onload = function onLoad() {
-    const clues = JSON.parse(req.responseText);
-    shuffle(clues);
-    fillBoard(clues);
-  };
-  const head = document.querySelector('.card h1');
-  explodeHeading(head);
+  const board = document.querySelector('.board');
+  buildEmptyBoard(board);
+  loadBoard(cluefile);
+  buildHeading();
+  const resetButton = document.querySelector('.controls .reset');
+  resetButton.addEventListener('click', (event)=> {
+    resetBoard(cluefile);
+  })
+  window.addEventListener('unload', (event) => {
+    saveBoard();
+  });
 }
 
 if (document.readyState != 'loading') {
