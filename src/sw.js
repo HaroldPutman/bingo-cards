b1507b0df4e98dd892f2de38736f4f307b632222
Harold Putman <harold.putman@lexmark.com>
2019-12-04 21:15:05 -0500

    Make it a PWA by adding Service Worker.

---
+++ src/sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'bingo-card-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/film-tropes.json',
+  '/css/app.css',
+  '/css/themes/default.css',
+  '/images/daub.jpg',
+  '/images/daub2.jpg'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
