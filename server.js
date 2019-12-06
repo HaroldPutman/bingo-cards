2d65195609b0b0b54fcf39050aaa3ec008f1320a
Harold Putman <harold.putman@lexmark.com>
2019-12-05 22:07:29 -0500

    Strip out Google Analytics when running from local.
    If you navigate to localhost:3000/ you get no analytics. If you do localhost:3000/index.html you get the analytics. This gives a great way to test analytics.

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
-var express = require('express')
-var app = express()
+const cheerio = require('cheerio');
+const express = require('express');
+const fs = require('fs');
+const app = express();
 
+app.get('/', (req, res) => {
+  res.contentType = 'text/html';
+  fs.readFile('www/index.html', (err, data) => {
+    if (err) {
+      throw err;
+    }
+    const $ = cheerio.load(data);
+    $('script.gtag').remove();
+    res.send($.html());
+  });
+});
 app.use(express.static('www'));
 app.use('/src', express.static('src'));
 app.listen(3000)
