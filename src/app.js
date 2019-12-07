c133b0123e1c2c59cf7cfe9472abf67d59664142
Harold Putman <harold.putman@lexmark.com>
2019-12-07 09:04:34 -0500

    Make it PWA (#21)
    * Change theme color.
    
    * Make it a PWA by adding Service Worker.
    
    * Add manifest.json

--- src/app.js
+++ src/app.js
@@ -263,3 +263,15 @@ if (document.readyState != 'loading') {
 } else {
   document.addEventListener('DOMContentLoaded', ready);
 }
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.min.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
