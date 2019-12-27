8ce5a9d348df9aa51fce31b62b13feaf02e52ef0
Harold Putman <harold.putman@lexmark.com>
2019-12-27 08:53:19 -0500

    Add "standard" for JS linting.

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const cheerio = require('cheerio');
-const express = require('express');
-const fs = require('fs');
-const app = express();
+const cheerio = require('cheerio')
+const express = require('express')
+const fs = require('fs')
+const app = express()
 
 app.get('/', (req, res) => {
-  res.contentType = 'text/html';
+  res.contentType = 'text/html'
   fs.readFile('www/index.html', (err, data) => {
     if (err) {
-      throw err;
+      throw err
     }
-    const $ = cheerio.load(data);
-    $('script.gtag').remove();
-    res.send($.html());
-  });
-});
-app.use(express.static('www'));
-app.use('/src', express.static('src'));
+    const $ = cheerio.load(data)
+    $('script.gtag').remove()
+    res.send($.html())
+  })
+})
+app.use(express.static('www'))
+app.use('/src', express.static('src'))
 app.listen(3000)
--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
+/* eslint-env browser  */
+/* globals gtag */
 /**
  * Tracks an event to Analytics.
  * @param {String} action The action name.
  * @param {Object} data The tag data.
  */
-function trackEvent(action, data) {
+function trackEvent (action, data) {
   if (typeof gtag === 'function') {
-    gtag('event', action, data);
+    gtag('event', action, data)
   }
 }
 
@@ -13,12 +15,12 @@ function trackEvent(action, data) {
  * Shuffles the contents of an array.
  * @param {Array} array
  */
-function shuffle(array) {
+function shuffle (array) {
   for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    let temp = array[j];
-    array[j] = array[i];
-    array[i] = temp;
+    const j = Math.floor(Math.random() * (i + 1))
+    const temp = array[j]
+    array[j] = array[i]
+    array[i] = temp
   }
 }
 
@@ -28,31 +30,31 @@ function shuffle(array) {
  * state of the board.
  * @returns {Array} An array of the found bingo patterns.
  */
-function findBingos(board) {
-  const bingos = [];
-  const row0 = parseInt('1111100000000000000000000', 2);
-  const col0 = parseInt('1000010000100001000010000', 2);
-  const leftDiag = parseInt('1000001000001000001000001', 2);
-  const rightDiag = parseInt('0000100010001000100010000', 2);
+function findBingos (board) {
+  const bingos = []
+  const row0 = parseInt('1111100000000000000000000', 2)
+  const col0 = parseInt('1000010000100001000010000', 2)
+  const leftDiag = parseInt('1000001000001000001000001', 2)
+  const rightDiag = parseInt('0000100010001000100010000', 2)
   if ((board & leftDiag) === leftDiag) {
-    bingos.push(leftDiag);
+    bingos.push(leftDiag)
   }
   if ((board & rightDiag) === rightDiag) {
-    bingos.push(rightDiag);
+    bingos.push(rightDiag)
   }
   for (let r = 0; r < 25; r += 5) {
-    let row = row0 >> r;
+    const row = row0 >> r
     if ((board & row) === row) {
-      bingos.push(row);
+      bingos.push(row)
     }
   }
   for (let c = 0; c < 5; c += 1) {
-    let col = col0 >> c;
+    const col = col0 >> c
     if ((board & col) === col) {
-      bingos.push(col);
+      bingos.push(col)
     }
   }
-  return bingos;
+  return bingos
 }
 
 /**
@@ -60,41 +62,41 @@ function findBingos(board) {
  * state of a cell.
  * @return {Number} The digitized board.
  */
-function digitizeBoard() {
+function digitizeBoard () {
   return Array.from(document.querySelectorAll('.board > div'))
     .reduce((acc, el) => {
-      acc = (acc << 1) | (el.classList.contains('marked') ? 1 : 0);
-      return acc;
-    }, 0);
+      acc = (acc << 1) | (el.classList.contains('marked') ? 1 : 0)
+      return acc
+    }, 0)
 }
 
 /**
  * Clear the bingo states.
  */
-function clearBingos() {
+function clearBingos () {
   Array.from(document.querySelectorAll('.bingo')).forEach((el) => {
-    el.classList.remove('bingo');
-  });
+    el.classList.remove('bingo')
+  })
 }
 
 /**
  * Mark all the bingos on the board.
  * @param {Array} bingos An array of zero or more bingo combinations.
  */
-function markBingos(bingos) {
+function markBingos (bingos) {
   bingos.forEach((mask) => {
-    let currentBit = parseInt('1000000000000000000000000', 2);
+    let currentBit = parseInt('1000000000000000000000000', 2)
     Array.from(document.querySelectorAll('.board > div'))
       .forEach((el) => {
         if (mask & currentBit) {
-          el.classList.add('bingo');
+          el.classList.add('bingo')
         }
-        currentBit >>= 1;
-      });
-  });
+        currentBit >>= 1
+      })
+  })
   if (bingos.length) {
-    const h1 = document.querySelector('.card h1');
-    h1.classList.add('bingo');
+    const h1 = document.querySelector('.card h1')
+    h1.classList.add('bingo')
   }
 }
 
@@ -103,12 +105,12 @@ function markBingos(bingos) {
  * mark the cells involved.
  * @returns {Number} The number of bingos found.
  */
-function checkForBingo() {
-  clearBingos();
-  const board = digitizeBoard();
-  const bingos = findBingos(board);
-  markBingos(bingos);
-  return bingos.length;
+function checkForBingo () {
+  clearBingos()
+  const board = digitizeBoard()
+  const bingos = findBingos(board)
+  markBingos(bingos)
+  return bingos.length
 }
 
 /**
@@ -116,62 +118,62 @@ function checkForBingo() {
  * survive a browser refresh. Because this can happen
  * on iOS when you switch apps and come back to Safari.
  */
-function saveBoard() {
-  const cells = document.querySelectorAll('.board > div');
+function saveBoard () {
+  const cells = document.querySelectorAll('.board > div')
   const serialized = Array.from(cells).map((el) => {
     return {
       t: el.innerText,
       m: el.classList.contains('marked')
     }
-  });
-  localStorage.setItem('saved', JSON.stringify(serialized));
-  localStorage.setItem('savedAt', Date.now());
+  })
+  localStorage.setItem('saved', JSON.stringify(serialized))
+  localStorage.setItem('savedAt', Date.now())
 }
 
 /**
  * Make the BINGO heading road by splitting the h1 text.
  * This lets the markup be more semantic.
  */
-function buildHeading() {
-  const el = document.querySelector('.card h1');
-  const text = el.innerText;
-  el.innerText = '';
+function buildHeading () {
+  const el = document.querySelector('.card h1')
+  const text = el.innerText
+  el.innerText = ''
   Array.from(text).forEach((ch) => {
-    const span = document.createElement('span');
-    span.innerText = ch;
-    el.appendChild(span);
-  });
+    const span = document.createElement('span')
+    span.innerText = ch
+    el.appendChild(span)
+  })
 }
 
 /**
  * Handle clicking on a cell by toggling the dauber mark.
  * @param {Object} event The click event.
  */
-function clickCell(event) {
-  const  marked = this.classList.toggle('marked');
+function clickCell (event) {
+  const marked = this.classList.toggle('marked')
   if (marked) {
     trackEvent('mark', {
       event_category: 'user',
       value: event.target.innerText
-    });
+    })
   }
-  const bingos = checkForBingo();
-  const rect = event.target.getBoundingClientRect();
-  const offset = (event.clientX - rect.left) / rect.width;
-  this.style.backgroundPositionX = (offset * 100)+'%';
+  checkForBingo()
+  const rect = event.target.getBoundingClientRect()
+  const offset = (event.clientX - rect.left) / rect.width
+  this.style.backgroundPositionX = (offset * 100) + '%'
 }
 
 /**
  * Create an empty bingo card.
  * @param {DomElement} board The container for bingo card.
  */
-function buildEmptyBoard(board) {
+function buildEmptyBoard (board) {
   for (let i = 0; i < 25; i++) {
-    const cell = document.createElement('div');
-    const div = document.createElement('div');
-    cell.appendChild(div);
-    cell.addEventListener('click', clickCell);
-    board.appendChild(cell);
+    const cell = document.createElement('div')
+    const div = document.createElement('div')
+    cell.appendChild(div)
+    cell.addEventListener('click', clickCell)
+    board.appendChild(cell)
   }
 }
 
@@ -181,12 +183,12 @@ function buildEmptyBoard(board) {
  *    `t`: Text in the cell
  *    `m`: The state of the dauber mark.
  */
-function fillCells(content) {
-  const cells = document.querySelectorAll('.board > div');
+function fillCells (content) {
+  const cells = document.querySelectorAll('.board > div')
   cells.forEach((cell, index) => {
-    const div = cell.querySelector('div');
-    div.innerText = content[index].t;
-    cell.classList.toggle('marked', content[index].m);
+    const div = cell.querySelector('div')
+    div.innerText = content[index].t
+    cell.classList.toggle('marked', content[index].m)
   })
 }
 
@@ -194,23 +196,23 @@ function fillCells(content) {
  * Loads content from the specified file to fill the board.
  * @param {String} tropeDefinitions URL of the JSON trope file.
  */
-function loadBoard(tropeDefinitions) {
-  req=new XMLHttpRequest();
-  req.open("GET", tropeDefinitions, true);
-  req.overrideMimeType("application/json");
-  req.send();
-  req.onload = function onLoad() {
-    const tropes = JSON.parse(req.responseText);
-    shuffle(tropes);
-    tropes[12] = 'FREE';
-    trackEvent('load', { value: tropeDefinitions });
+function loadBoard (tropeDefinitions) {
+  const req = new XMLHttpRequest()
+  req.open('GET', tropeDefinitions, true)
+  req.overrideMimeType('application/json')
+  req.send()
+  req.onload = function onLoad () {
+    const tropes = JSON.parse(req.responseText)
+    shuffle(tropes)
+    tropes[12] = 'FREE'
+    trackEvent('load', { value: tropeDefinitions })
     fillCells(tropes.map((text) => {
       return {
         t: text,
         m: false
-      };
-    }));
-  };
+      }
+    }))
+  }
 }
 
 /**
@@ -218,60 +220,60 @@ function loadBoard(tropeDefinitions) {
  * a trope file.
  * @param {String} tropeDefinitions URL JSON file to get new tropes.
  */
-function populateBoard(tropeDefinitions) {
-  let age = 0;
-  const saved = localStorage.getItem('saved');
+function populateBoard (tropeDefinitions) {
+  let age = 0
+  const saved = localStorage.getItem('saved')
   if (saved) {
-    const savedAt = Number(localStorage.getItem('savedAt'));
-    age = Math.floor((Date.now() - savedAt) / 1000);
+    const savedAt = Number(localStorage.getItem('savedAt'))
+    age = Math.floor((Date.now() - savedAt) / 1000)
   }
   if (!saved || (age > 7200)) {
-    loadBoard(tropeDefinitions);
+    loadBoard(tropeDefinitions)
   } else {
-    trackEvent('restore', { value: age });
-    fillCells(JSON.parse(saved));
-    checkForBingo();
+    trackEvent('restore', { value: age })
+    fillCells(JSON.parse(saved))
+    checkForBingo()
   }
 }
 
 /**
  * Handles the Document Ready event.
  */
-function ready() {
-  const searchParams = new URLSearchParams(window.location.search);
+function ready () {
+  const searchParams = new URLSearchParams(window.location.search)
   let tropeDefinitions = 'film-tropes.json'
   if (searchParams.has('tropes')) {
-    tropeDefinitions = searchParams.get('tropes');
+    tropeDefinitions = searchParams.get('tropes')
   }
-  const board = document.querySelector('.board');
-  buildEmptyBoard(board);
-  populateBoard(tropeDefinitions);
-  buildHeading();
-  const resetButton = document.querySelector('.controls .reset');
-  resetButton.addEventListener('click', (event)=> {
-    trackEvent('reset', { event_category: 'user' });
-    clearBingos();
-    loadBoard(tropeDefinitions);
+  const board = document.querySelector('.board')
+  buildEmptyBoard(board)
+  populateBoard(tropeDefinitions)
+  buildHeading()
+  const resetButton = document.querySelector('.controls .reset')
+  resetButton.addEventListener('click', (event) => {
+    trackEvent('reset', { event_category: 'user' })
+    clearBingos()
+    loadBoard(tropeDefinitions)
   })
   window.addEventListener('unload', (event) => {
-    saveBoard();
-  });
+    saveBoard()
+  })
 }
 
-if (document.readyState != 'loading') {
-  ready();
+if (document.readyState !== 'loading') {
+  ready()
 } else {
-  document.addEventListener('DOMContentLoaded', ready);
+  document.addEventListener('DOMContentLoaded', ready)
 }
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.min.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/sw.min.js').then(function (registration) {
       // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope)
+    }, function (err) {
       // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
+      console.log('ServiceWorker registration failed: ', err)
+    })
+  })
 }
